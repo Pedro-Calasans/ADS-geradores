ff58999c646096ea7f8566f613b3355e49ffb652
pedro guilherme <pedro.calasans@aluno.ifsp.edu.br>
2023-08-19 03:13:41 -0300

    arrumando o index para enviar e-mail

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 const nodemailer = require("nodemailer");
 
+const transport = nodemailer.createTransport({
+    host: '',
+    port: 587,
+    secure: false,
+    auth:{
+
+    }
+});
+
+transport.sendMail({
+    from: "",
+    to: "",
+    subject: "",
+    text: "",
+})
