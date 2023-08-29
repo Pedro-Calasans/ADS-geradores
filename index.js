199deefa380ab726d3e920fbbd01deab80b15642
pedro guilherme <pedro.calasans@aluno.ifsp.edu.br>
2023-08-28 22:06:55 -0300

    site quase pronto

--- index.js
+++ index.js
@@ -15,3 +15,5 @@ transport.sendMail({
     subject: "",
     text: "",
 })
+
+const mensagem = ("Olá! Bem-vindo ao Grupo ADS Geradores.Se você está em busca de serviços confiáveis para seus grupos geradores, você está no lugar certo. Nossa equipe especializada oferece uma variedade de serviços, incluindo:- Manutenção corretiva e preventiva de grupo gerador.- Automação e retrofit para sistemas de energia.- Limpeza de tanque de combustível.- Análise de combustível para garantir o melhor desempenho do seu gerador.Se você tem alguma pergunta, precisa de mais informações ou deseja solicitar um orçamento, não hesite em nos enviar uma mensagem. Estamos aqui para ajudar!")
